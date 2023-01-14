424ae0698297ee4ec906f2186c8a7cadb89aa1ae
rakesh kumar sahu <sanjayrakeshgupta@gmail.com>
2023-01-14 16:35:28 +0530

    java code oprerator

---
+++ operators/A.java
@@ -0,0 +1,19 @@
+//package operators;
+
+public class A {
+
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+        System.out.println(a*b);
+        System.out.println(a + b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a - b);
+
+        // == is equals to
+        // ! not equal
+        // > greater than
+
+    }
+
+}
--- operators/Operator.java
+++ operators/Operator.java
@@ -1,5 +1,21 @@
 package operators;
 
 public class Operator {
-    
+    // Arithmetic Operator
+    // relations operators
+    // logical Operator
+    // Assigment operators
+    // unary operators
+    // bitwise operators
+    // 3+9=10
+    // 3-operands
+    // +-operators
+
+    // % -Modulus 
+    // 7/3=2,10/2=5divion{7%3=1,10%2=0}
+    // ++ -Increment  10,11
+    // -- -Decrement 10,9
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
