e5562e978e553d3db048a895768fd2a2ba5f551c
rakesh kumar sahu <sanjayrakeshgupta@gmail.com>
2023-02-05 21:02:47 +0530

    add

---
+++ exception handling/a.java
@@ -0,0 +1,9 @@
+class a {
+    public static void main(String[] args) {
+        String str = "sanjay";
+        for (int index = str.length() - 1; index >= 0; index--) {
+            System.out.println(str.charAt(index));
+
+        }
+    }
+}
